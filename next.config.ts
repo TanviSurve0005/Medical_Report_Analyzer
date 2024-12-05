484e6b6d0205d7850ef282c3f533a4fd72decc46
Shubham Verma <shubhamverma6351@gmail.com>
2024-12-05 20:16:05 +0530

    fix: update Next.js configuration to support onnxruntime-node as an external dependency
    - Modified next.config.ts to include onnxruntime-node in the webpack externals for server builds.
    - Changed export method from ES module to CommonJS for compatibility.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (config: { externals: string[]; }, { isServer }: any) => {
+    if (isServer) {
+      config.externals.push("onnxruntime-node");
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
